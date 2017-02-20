7b5f832e71355f6bf963d52cdae74dbc3916fe0b
gardian <gardiangy@gmail.com>
2017-02-20 21:34:35 +0100

    backend init

---
+++ backend/src/main/java/hu/voga/FloraApplication.java
@@ -0,0 +1,12 @@
+package hu.voga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloraApplication.class, args);
+	}
+}
