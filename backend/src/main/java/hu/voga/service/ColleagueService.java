bc1c6daae2c0f4631dae4aeeaa97d1c2a5814cee
gardian <gardiangy@gmail.com>
2017-02-22 22:16:14 +0100

    backend

---
+++ backend/src/main/java/hu/voga/service/ColleagueService.java
@@ -0,0 +1,33 @@
+package hu.voga.service;
+
+import hu.voga.dao.ColleagueRepository;
+import hu.voga.dao.NewsRepository;
+import hu.voga.entity.Colleague;
+import hu.voga.entity.News;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by GGyuri on 2017. 02. 20..
+ */
+@Service
+public class ColleagueService {
+
+    @Autowired
+    private ColleagueRepository colleagueRepository;
+
+    public Colleague save(Colleague colleague){
+        return colleagueRepository.save(colleague);
+    }
+
+    public Colleague findOne(Long id){
+        return colleagueRepository.findOne(id);
+    }
+
+    public List<Colleague> findAll(){
+        return (List<Colleague>) colleagueRepository.findAll();
+    }
+
+}
