7c2500dac69173eb0338f157e04df62dc1bf5f24
Völgyi Dániel <lofutty23xx@gmail.com>
2017-02-25 21:24:17 +0100

    header

---
+++ frontend/flora/src/app/main/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flora-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onNewsLetter(){
+  }
+
+}
