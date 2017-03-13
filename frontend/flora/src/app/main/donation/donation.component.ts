be90e01b50c008018bbe28c13c2a8e109e033c41
Völgyi Dániel <lofutty23xx@gmail.com>
2017-03-13 21:10:19 +0100

    donation

---
+++ frontend/flora/src/app/main/donation/donation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flora-donation',
+  templateUrl: './donation.component.html',
+  styleUrls: ['./donation.component.css']
+})
+export class DonationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
