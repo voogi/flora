1cfe849ecbff4b196e9c899ab3760f4794b1ab3c
gardian <gardiangy@gmail.com>
2017-04-23 21:06:53 +0200

    backend security

--- backend/src/main/java/hu/voga/config/WebSecurityConfig.java
+++ backend/src/main/java/hu/voga/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package hu.voga.config;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,11 +13,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/colleague/**", "/api/event/**" , "/api/knowledge/**" , "/api/news/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/newsletter").permitAll()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.GET, "/api/newsletter/**", "/api/volunteer").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/colleague/**", "/api/event/**" , "/api/knowledge/**" , "/api/news/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/colleague/**", "/api/event/**" , "/api/knowledge/**" , "/api/news/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/api/colleague/**", "/api/event/**" , "/api/knowledge/**" , "/api/news/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/login", "/api/colleague/**", "/api/event/**" , "/api/knowledge/**" , "/api/news/**").permitAll()
                 .and()
                 .csrf().disable()
                 .httpBasic();
