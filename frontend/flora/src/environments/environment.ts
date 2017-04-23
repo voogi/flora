747a6815e880bc3d99c44ebcca0c60dae765699c
Völgyi Dániel <lofutty23xx@gmail.com>
2017-04-23 21:12:46 +0200

    css

--- frontend/flora/src/environments/environment.ts
+++ frontend/flora/src/environments/environment.ts
@@ -2,10 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
-  bUrl : "http://localhost:8080",
+  bUrl : window.location.host,
   imagesUrl: "http://localhost:8080/static/images/",
   cvsUrl: "http://localhost:8080/static/cvs/"
 };
